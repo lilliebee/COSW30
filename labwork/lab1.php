3a9cc07fbe921724c01e589fef373a8cc3838537
EC2 Default User <ec2-user@ip-172-31-30-47.ec2.internal>
2019-09-10 15:04:40 +0000

    my first commit

---
+++ labwork/lab1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Labwork 1:Hello World</title>
+        /*
+        lab1.php
+        Karen Candelario
+        8/29/19
+        */
+        <style type="text/css">
+            body {
+                background-color:#c5e3de;
+                font: Arial;
+                
+         
+            }
+        </style>
+    </head>
+    <body>
+        
+        <h1>All About Me</h1>
+        <p>I am Karen, I am fairly new to web development but very excited to learn all aspects of it. I am a mom of 2 under 3 so apart from school my life mostly revolves around them. I enjoy hiking, cooking, and photography.</p>
+        <a href="https://github.com/lilliebee">Connect with me on GitHub!</a>
+       
+       
+       
+
+<?php 
+print "<p>My favorite website is <a href=\"http://www.youtube.com\">Youtube</a>hands down I use it for just about everything and it's especially been a huge help ever since I decided to learn about the computer field. Whenever I am not watching videos in Youtube I am usually on Pinterest <a href=\"http://www.pinterest.com\">Pinterest</a>.</p>";
+print "<p> To be completely honest this first lab was really difficult for me. I struggled the most with the php links and pushing it to git. I am hoping with more practice and my book I'll be able to understand everything a bit more."
+?>
+
+
+
+
+    
+     
+        
+    </body>
+</html>
+
+
+
+  
+    
