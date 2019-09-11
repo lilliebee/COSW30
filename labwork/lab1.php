abb8c70fd7c48d90afe1922e4c7348d431253da5
EC2 Default User <ec2-user@ip-172-31-30-47.ec2.internal>
2019-09-11 12:51:18 +0000

    second commit

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -2,32 +2,38 @@
 <html>
     <head>
         <title>Labwork 1:Hello World</title>
-        /*
-        lab1.php
+        /* lab1.php
         Karen Candelario
-        8/29/19
-        */
+        8/29/19 */
+        
         <style type="text/css">
             body {
                 background-color:#c5e3de;
                 font: Arial;
+                margin:50px;
                 
-         
+            }
+            
+            h1 {
+                text-align:center;
+                border-bottom: 10px;
+                border-bottom-color: black;
+                border-bottom-style: solid;
             }
         </style>
     </head>
     <body>
         
         <h1>All About Me</h1>
-        <p>I am Karen, I am fairly new to web development but very excited to learn all aspects of it. I am a mom of 2 under 3 so apart from school my life mostly revolves around them. I enjoy hiking, cooking, and photography.</p>
+        <p>I am Karen, I have been a student at Long Beach City College for the past year. I am fairly new to web development but completely fell in love with it after taking COSW10, I am very excited to learn all aspects of the field and hopefully get to one day apply it to land myself a job at Nordy's as a web designer. I enjoy hiking, cooking, photographing my babies and fur babies, and spending time with them in general.  </p>
         <a href="https://github.com/lilliebee">Connect with me on GitHub!</a>
        
        
        
 
 <?php 
-print "<p>My favorite website is <a href=\"http://www.youtube.com\">Youtube</a>hands down I use it for just about everything and it's especially been a huge help ever since I decided to learn about the computer field. Whenever I am not watching videos in Youtube I am usually on Pinterest <a href=\"http://www.pinterest.com\">Pinterest</a>.</p>";
-print "<p> To be completely honest this first lab was really difficult for me. I struggled the most with the php links and pushing it to git. I am hoping with more practice and my book I'll be able to understand everything a bit more."
+print "<p>My favorite website is <a href=\"http://www.youtube.com\">Youtube</a> hands down I use it for just about everything and it's especially been a huge help ever since I decided to learn about the computer field. Whenever I am not watching videos in Youtube I am usually on <a href=\"http://www.pinterest.com\">Pinterest</a> attempting DIY's.</p>";
+print "<p> This first assignment was pretty dang challenging. I struggled with pushing the file to git and then afterwards couldn't figure out why heroku deployed an empty web page after extensive google searches I hoped onto the class discord chat and found the answer *dramatic pause* I wasn't typing in the whole link correctly. Now that I've figured out my mistakes I am hoping I'll be able to understand everything a bit more."
 ?>
 
 
