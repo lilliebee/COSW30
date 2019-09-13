e4968ca727f68a1b380f91ea656ccdaee730a4c3
EC2 Default User <ec2-user@ip-172-31-30-47.ec2.internal>
2019-09-13 17:56:44 +0000

    hw2 second commit

---
+++ homework/homework2/hw2.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Homework2:Political Candidate </title>
+    </head>
+    
+ <body>
+        <h1> Support your favorite The Office character for President! </h1>
+        
+        </body>
+        
