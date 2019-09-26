79844144daf70994137bf5be11440eab36e745b6
EC2 Default User <ec2-user@ip-172-31-30-47.ec2.internal>
2019-09-26 23:04:37 +0000

    tuition cost

--- homework/homework3/process_tuition.php
+++ homework/homework3/process_tuition.php
@@ -6,12 +6,27 @@ $resident = $_POST['resident'];
 $units = $_POST['units'];
 $services = $_POST['units'];
 $parking = $_POST['units'];
+
+
+
+
 $costoftuition = $resident * $units;
+$totalcostoftuition = $costoftuition + $services + $parking;
 
 
 
 
-echo "<p> Cost of tuition: $units units x $resident = $costoftuition </p>";
+echo "<p> Cost of tuition: $units units x  $ $resident = $ $costoftuition </p>";
+
+echo "<p> Mandatory Student Health Fee: $ 19 </p>";
+
+echo "<p> College Services Card: $services ";
+
+echo "<p> Parking Permit: $parking ";
+
+echo "<p> Total College Balance Due: $totalcostoftuition ";
+
+
 
 
 
