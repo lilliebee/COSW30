e44c2fbe39d4024922dda4ba1de336fd740aa89f
EC2 Default User <ec2-user@ip-172-31-30-47.ec2.internal>
2019-09-27 21:15:35 +0000

    third commit

--- homework/homework4/quote_process.php
+++ homework/homework4/quote_process.php
@@ -1,14 +1,27 @@
 <?php
 
 $quote = $_POST['quote'];
-$quote = array();
-$quote[0]='';
-$quote[1]='';
-$quote[2]='';
-$quote[3]='';
-$quote[4]='';
+
+$quote = explode('', filter_input(INPUT_POST, 'quote'));
+
+$array = $quote;
+
+foreach ($array as $value) {
+    echo "$value<br>";
+}
+
+sort($array);
+
+foreach ($array as $value)
+{
+    echo "$value <br>";
+}
+
+arsort($array);
 
 
 
-echo "<p> My Favorite quote: $quote";
+
+
+
 ?>
