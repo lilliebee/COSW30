84da343fa6dc77e523a3a457ccd44062aefe7744
EC2 Default User <ec2-user@ip-172-31-30-47.ec2.internal>
2019-10-01 05:04:58 +0000

    login fix

--- homework/homework5/footer.php
+++ homework/homework5/footer.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <footer>
-            <p>	&copy; September 25, 2019, COSW 30 Karen Candelario </p>
+            <p>	&copy; September 29, 2019, COSW 30 Karen Candelario </p>
         </footer>
           
 </html>
