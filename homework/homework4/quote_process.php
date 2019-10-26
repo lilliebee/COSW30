f8a35e1410d079c1cd62d0b81dc9c2bba9456915
EC2 Default User <ec2-user@ip-172-31-30-47.ec2.internal>
2019-10-26 06:13:01 +0000

    random words fix

--- homework/homework4/quote_process.php
+++ homework/homework4/quote_process.php
@@ -11,14 +11,43 @@ echo "<p class=\"underlineText\">Your Original List Is: </p>";
     echo "<p>Word Count: " .  count($inputList) . "</p>";
     echo "<br><br><br>";
 
-
-
-
-
-
-
-
-
-
+//Output list alphabetized 
+    //natcasesort used for case-insensitive
+    echo "<p class=\"underlineText\">Alphabetized Version Of List: </p>";
+    natcasesort($inputList);
+    foreach($inputList as $value){
+        echo "<p>$value</p>";
+    }
+    echo "<br><br><br>";
+    
+     //Output the list reverse alphabetized 
+    echo "<p class=\"underlineText\">Reverse Alphabetized Version Of List: </p>";
+    rsort($inputList);
+    foreach($inputList as $value){
+        echo "<p>$value</p>";
+    }
+    echo "<br><br><br>";
+    //Add three random words to end of list
+    //interesting,amused,request
+    array_push($inputList, 'interesting', 'request', 'amused');
+    //Output new list
+    //include word count
+    echo "<p class=\"underlineText\">Reverse Alphabetized Version Plus Three Random Words: </p>";
+    foreach($inputList as $value){
+        echo "<p>$value</p>";
+    }
+    echo "<p>Word Count: " .  count($inputList) . "</p>";
+    echo "<br><br><br>";
+    
+    //Remove first three words of list
+    array_splice($inputList, 0, 3);
+    //Output the new list
+    //include word count
+    echo "<p class=\"underlineText\">Reverse Alphabetized Version With First Three Words Removed: </p>";
+    foreach($inputList as $value){
+        echo "<p>$value</p>";
+    }
+    echo "<p>Word Count: " .  count($inputList) . "</p>";
+    echo "<br><br><br>";
 
 ?>
